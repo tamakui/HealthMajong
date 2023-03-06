ef53f2bb613974701ff8c5a40c2fdcddd9aa5ce2
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-03-06 16:38:08 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     patch '/end_users/withdraw' => 'end_users#withdraw'
     resources :scorings, only: [:index, :show]
     resources :mahjong_hands, only: [:index, :show]
-    resources :recruitments, only: [:new, :index, :show, :edit]
+    resources :recruitments, only: [:new, :index, :show, :edit, :create, :destroy]
   end
   
   #管理者
@@ -25,43 +25,12 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
   
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    resources :end_users, only: [:index, :show, :edit, :update]
+    resources :mahjong_hands, only: [:new, :index, :show, :edit, :create, :update]
+    resources :genres, only: [:index, :edit. :create, :update]
+  end
   
-  namespace :admin do
-    get 'end_users/index'
-    get 'end_users/show'
-    get 'end_users/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'mahjong_hands/new'
-    get 'mahjong_hands/index'
-    get 'mahjong_hands/show'
-    get 'mahjong_hands/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'end_users/show'
-    get 'end_users/edit'
-  end
-  #namespace :public do
-    #get 'scorings/index'
-    #get 'scorings/show'
-  #end
-  #namespace :public do
-    #get 'mahjong_hands/index'
-    #get 'mahjong_hands/show'
-  #end
-  #namespace :public do
-    #get 'recruitments/new'
-    #get 'recruitments/index'
-    #get 'recruitments/show'
-    #get 'recruitments/edit'
-  #end
- 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
