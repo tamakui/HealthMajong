f613ed09935d982327f075af93c8004fbc5c41d8
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-03-08 18:39:36 +0000

    [Add]admin_mahjong_hand

--- app/controllers/admin/mahjong_hands_controller.rb
+++ app/controllers/admin/mahjong_hands_controller.rb
@@ -1,13 +1,18 @@
 class Admin::MahjongHandsController < ApplicationController
   def new
+    @mahjong_hand = MahjongHand.new
   end
 
   def index
+    @mahjong_hand = MahjongHand.new
+    @mahjong_hands = MahjongHand.all
   end
 
   def show
+    @mahjong_hand = MahjongHand.find(params[:id])
   end
 
   def edit
+    @mahjong_hand = MahjongHand.fin(params[:id])
   end
 end
