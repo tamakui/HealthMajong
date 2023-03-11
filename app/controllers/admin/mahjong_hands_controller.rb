dbf7d02e676b1fa70bf6674956804ddcabc06c97
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-03-11 14:55:12 +0000

    [Add]adminMahjongHand

--- app/controllers/admin/mahjong_hands_controller.rb
+++ app/controllers/admin/mahjong_hands_controller.rb
@@ -13,6 +13,27 @@ class Admin::MahjongHandsController < ApplicationController
   end
 
   def edit
-    @mahjong_hand = MahjongHand.fin(params[:id])
+    @mahjong_hand = MahjongHand.find(params[:id])
+  end
+  
+  def create
+    @mahjong_hand = MahjongHand.new(mahjong_hand_params)
+    @mahjong_hand.save
+    redirect_to admin_mahjong_hand_path(@mahjong_hand)
+  end
+  
+  def update
+    @mahjong_hand = MahjongHand.find(params[:id])
+    if @mahjong_hand.update(mahjong_hand_params)
+      redirect_to admin_mahjong_hand_path(@mahjong_hand.id)
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def mahjong_hand_params
+    params.require(:mahjong_hand).permit(:name, :describe, :image, :genre_id)
   end
 end
--- app/models/mahjong_hand.rb
+++ app/models/mahjong_hand.rb
@@ -1,2 +1,3 @@
 class MahjongHand < ApplicationRecord
+    has_one_attached :image
 end
