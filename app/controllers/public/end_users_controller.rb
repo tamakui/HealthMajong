081a4bdaca8348d931b7d0855c6fbe96a7bd6c3d
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-03-12 08:41:11 +0000

    [Add]enduser,show,edit

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,8 +1,21 @@
 class Public::EndUsersController < ApplicationController
+  before_action :authenticate_end_user!
+  
   def show
+    @end_user = current_end_user
   end
 
   def edit
+    @end_user = current_end_user
+  end
+  
+  def update
+    @end_user = current_end_user
+    if @end_user.update(end_user_params)
+      redirect_to end_users_my_page_path
+    else
+      render 'edit'
+    end
   end
   
   def unsubscribe
@@ -15,4 +28,19 @@ class Public::EndUsersController < ApplicationController
     reset_session
     redirect_to root_path
   end
+  
+  def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  private
+  
+  def end_user_params
+    prams.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :login_name, :profile_image, :is_deleted)
+  end
+  
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,4 +3,9 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :login_name, presence: true, length: { maximum: 20 }
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
