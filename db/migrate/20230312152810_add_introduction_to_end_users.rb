dc16411346cf91c996ce7b57c1673da62a7d53f1
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-03-12 17:56:02 +0000

    [Add]mypage

---
+++ db/migrate/20230312152810_add_introduction_to_end_users.rb
@@ -0,0 +1,5 @@
+class AddIntroductionToEndUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :end_users, :introduction, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_11_053207) do
+ActiveRecord::Schema.define(version: 2023_03_12_152810) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2023_03_11_053207) do
     t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
