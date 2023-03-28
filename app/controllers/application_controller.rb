5a73c9f36ebf2b07addf46e0b83c99aeb8b615a3
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-03-28 06:18:47 +0000

    [Add]application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :data_not_found
+      
+  private
+  
+  def data_not_found
+  flash[:error] = "データが存在しません"
+  redirect_back(fallback_location: root_path)
+  end
   
 end
